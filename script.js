494769fe809f937a73a221bbf469d972a379652c
Mari <marietec.lima244@gmail.com>
2026-02-27 13:55:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let quantidadeCliques = document.querySelector('.numeroCliques')
 
 function incrementarCliques() {
+    const miau = new Audio("dragon-studio-cat-meow-401729.mp3")
     quantidadeCliques.innerHTML = parseFloat(quantidadeCliques.innerHTML) + 1
+    miau.play()
 }
