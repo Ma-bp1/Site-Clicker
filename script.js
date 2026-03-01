34a474221c396d714855214167b8b5795555732d
Mari <marietec.lima244@gmail.com>
2026-03-01 20:00:10 -0300

    teste 1

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function incrementarCliques() {
     quantidadeCliques.innerHTML = parseFloat(quantidadeCliques.innerHTML) + 1
     miau.play()
 }
+
+export const upgrades = [
+    {
+        title:"Ratinhos trabalhadores",
+        custo:"10",
+        description:"Terá ratos para trabalhar pra você (autoclicker)",
+    },
+
+    {
+        title:"Leite",
+        custo:"20",
+        description:"",
+    },
+
+    {
+        title:"Dar ao nome do seu gato",
+        custo:"100",
+        description:"Você pode nomear seu bichano",
+    },
+
+    {
+        title:"Ronronar",
+        custo:"150",
+        description:"Seu pet comecou a gostar de você, ele irá ronronar quando você der carinho",
