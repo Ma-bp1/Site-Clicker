c15bd4480c6bf73cb80f3099bd3d104a02c05b2d
Mari <marietec.lima244@gmail.com>
2026-03-01 22:57:07 -0300

    mais edicoes

--- script.js
+++ script.js
@@ -4,50 +4,71 @@ export function displayCliques() {
     contarCliques.innerHTML = parseFloat(contarCliques.innerHTML) + 1 
 }
 
-document.getElementById('gato').addEventListener('click', contarCliques)
-
-function comprarUpgrade(indice){
-    let podeComprar = false;
-    
-
-    if (document.getElementById('labelClique').innerHTML >= {upgrades.custo}) {
-        podeComprar = true;
-    }
-    else podeComprar() = false;
-}
-
-document.querySelector('.cardContainer').addEventListener('click', comprarUpgrade())
-
-//upgrades e suas funcionalidades abaixo:
-function upgradeRatinhos() {
-    setInterval(()=>{
-        displayCliques();
-    }, 100);
-}
-
+document.getElementById('gato').addEventListener('click', displayCliques)
 
 export const upgrades = [
     {
         titulo:"Ratinhos Trabalhadores",
         custo:"10",
-        description:"Terá ratos para trabalhar pra você (autoclicker)",
+        descricao:"Terá ratos para trabalhar pra você (autoclicker)",
+        acao: upgradeRatinhos
     },
 
     {
         titulo:"Leite",
         custo:"20",
-        description:"Faz o gato ficar feliz (multiplica 2x o autoclicker)",
+        descricao:"Faz o gato ficar feliz (multiplica 2x o autoclicker)",
+        acao: upgradeLeite
     },
 
     {
         titulo:"Dar nome ao seu gato",
         custo:"100",
-        description:"Você pode nomear seu bichano",
+        descricao:"Você pode nomear seu bichano",
+        acao: upgradeNomeGato
     },
 
     {
         titulo:"Ronronar",
         custo:"150",
-        description:"Seu pet comecou a gostar de você, ele irá ronronar quando você der carinho",
+        descricao:"Seu pet comecou a gostar de você, ele irá ronronar quando você der carinho",
+        acao: upgradeRonronar
     }
 ]
+
+function comprarUpgrade(indice){
+    const upgrade = upgrades[indice];
+    const custo = parseFloat(upgrade.custo);
+    
+    if (document.getElementById('labelClique').innerHTML >= custo) {
+        document.getElementById('labelClique').innerHTML -= custo;
+
+        upgrade.acao();
+    }
+    else {
+        alert("Cliques insuficientes! Clique um pouco mais :3")
+    }
+}
+
+//upgrades e suas funcionalidades abaixo:
+function upgradeRatinhos() {
+    var tempoAlteravel = 1000;
+
+    setInterval(()=>{
+        displayCliques();
+    }, tempoAlteravel);
+}
+
+function upgradeLeite() {
+    console.log('teste')
+}
+
+function upgradeNomeGato() {
+    console.log('teste')
+}
+
+function upgradeRonronar() {
+    console.log('teste')
+}
+
+
